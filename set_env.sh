61928aa104a6ab05eb2d6ab7e514f6c58ff087d2
Meshak Ahoka <ahoka@Mshak-Ahoka.localdomain>
2022-11-14 19:16:15 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,21 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# export POSTGRES_USERNAME=udagrams
+# export POSTGRES_PASSWORD=postgrespostgres
+# export POSTGRES_HOST=udagram.cnrbz6req7bc.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=udagram-mono
+# export AWS_BUCKET=arn:aws:s3:::udagram-mono
+# export AWS_REGION=us-east-1
+# export AWS_PROFILE=Ahoka3
+# export JWT_SECRET=testing
+# export URL=http://localhost:8100
+
+export POSTGRES_USERNAME=udagrams
+export POSTGRES_PASSWORD=udagramsudagrams
+export POSTGRES_HOST=udagrams.cnrbz6req7bc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagrams
+export AWS_BUCKET=arn:aws:s3:::udagram-mono
+export AWS_REGION=us-east-1
+export AWS_PROFILE=Ahoka3
+export JWT_SECRET=testing
+export URL=http://localhost:8100
